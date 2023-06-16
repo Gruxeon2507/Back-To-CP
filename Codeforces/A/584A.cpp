513f3ee07ed0d3afc15c279fbf589b434e0b81ad
ducseo2022 <khieuminhduc2012@gmail.com>
2023-06-16 16:01:28 +0700

    add cf problem

---
+++ Codeforces/A/584A.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n,t;
+    cin >> n >> t;
+    if(n==1&&t==10){
+        cout << -1 << endl;
+        return;
+    }
+
+    if(t==10){
+        cout << 1;
+        for(int i=0;i<n-1;i++){
+            cout << 0;
+        }
+        cout << endl;
+        return;
+    }
+    else{
+        for(int i=0;i<n;i++){
+            cout <<  t;
+        }
+        cout << endl;
+    }
+
+}
+int main() {
+    int t;
+//    std::cin >> t;
+    t=1;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
--- main.cpp
+++ main.cpp
@@ -2,41 +2,27 @@
 using namespace std;
 
 void solve(){
-   int n;
-   cin >> n;
-   vector<int> a(n);
-   for(int i=0;i<n;i++){
-       cin >> a[i];
-   }
-   int l=0;
-   for(int i=0;i<n-1;i++){
-       if(a[i]<a[i+1]){
-           l=i;
-           break;
-       }
-   }
-   int r=l+1;
-   for(int i=r;i<n;i--){
-       if(a[i]<a[i-1]){
-           r=i;
-           break;
-       }
-   }
-   if(n==1){
-       cout << "YES\n";
-       cout << "1 1\n";
+   int n,t;
+   cin >> n >> t;
+   if(n==1&&t==10){
+       cout << -1 << endl;
        return;
    }
-    reverse(a.begin()+l,a.begin()+r);
-    for (int i = 1; i < n; ++i) {
-        if (a[i] < a[i - 1]) {
-            cout << "NO" << endl;
-            return;
-        }
-    }
-    cout <<"YES\n";
-    cout << l+1 <<" " << r <<endl;
-    return;
+
+   if(t==10){
+       cout << 1;
+       for(int i=0;i<n-1;i++){
+           cout << 0;
+       }
+       cout << endl;
+       return;
+   }
+   else{
+       for(int i=0;i<n;i++){
+           cout <<  t;
+       }
+       cout << endl;
+   }
 
 }
 int main() {
